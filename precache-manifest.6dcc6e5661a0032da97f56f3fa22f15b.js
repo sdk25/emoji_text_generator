ad118c57b829d2b0e486841160f36b8e13b5bcc5
Dmitry Saltykov <25DimoN25@spark-mail.ru>
2020-05-12 18:01:20 +0300

    Updates

---
+++ precache-manifest.6dcc6e5661a0032da97f56f3fa22f15b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70c8a6bfb84df8bd40bce3e86a24f27e",
+    "url": "/emoji_text_generator/index.html"
+  },
+  {
+    "revision": "cb32e7034d561e25b413",
+    "url": "/emoji_text_generator/static/js/2.308f21c0.chunk.js"
+  },
+  {
+    "revision": "f9788c56a02fc454b842",
+    "url": "/emoji_text_generator/static/js/main.09da2cb8.chunk.js"
+  },
+  {
+    "revision": "76b46a476762c760dcb7",
+    "url": "/emoji_text_generator/static/js/runtime~main.ce804f2b.js"
+  }
+]);

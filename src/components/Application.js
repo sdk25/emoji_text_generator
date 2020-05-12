4842c37f161490ecf8e910e15bd6b638e95561e7
Dmitry Saltykov <25DimoN25@spark-mail.ru>
2020-05-12 18:08:12 +0300

    table spacing fix

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,6 +2,16 @@ import React, {useEffect, useState} from "react"
 import TextField from "@material-ui/core/TextField"
 import Grid from "@material-ui/core/Grid"
 import {charTable} from "constants/charTable"
+import Card from "@material-ui/core/Card";
+import CardContent from '@material-ui/core/CardContent';
+import makeStyles from "@material-ui/core/styles/makeStyles";
+
+const useStyles = makeStyles({
+    rootCard: {
+        maxWidth: 800,
+        backgroundColor: "#f0ffef"
+    }
+})
 
 export default function Application() {
 
@@ -10,8 +20,11 @@ export default function Application() {
     const [inputText, setInputText] = useState("жесть")
     const [outputText, setOutputText] = useState("")
 
+    const classes = useStyles();
+
     useEffect(() => {
-        const newOutputText = inputText.toLowerCase()
+        const newOutputText = inputText
+            .toLowerCase()
             .split("")
             .map(char => charTable[char] || [])
             .reduce((rows, charArray) => { // TODO make it pure
@@ -28,47 +41,47 @@ export default function Application() {
     }, [innerChar, outerChar, inputText])
 
     return (
-        <Grid container spacing={3} style={{maxWidth: 800}}>
-            <Grid item xs={12}>
-                <TextField
-                    label="output"
-                    fullWidth
-                    margin="normal"
-                    multiline
-                    rows={7}
-                    variant="outlined"
-                    value={outputText}
-                />
-            </Grid>
-            <Grid item xs={8}>
-                <TextField
-                    label="input"
-                    fullWidth
-                    margin="normal"
-                    value={inputText}
-                    onChange={event => setInputText(event.target.value)}
-                />
-            </Grid>
-            <Grid item xs={4}>
-                <Grid item xs={12}>
-                    <TextField
-                        label="inner"
-                        fullWidth
-                        margin="normal"
-                        value={innerChar}
-                        onChange={event => setInnerChar(event.target.value)}
-                    />
+        <Card variant="outlined" className={classes.rootCard}>
+            <CardContent>
+                <Grid container spacing={2}>
+                    <Grid item xs={12}>
+                        <TextField
+                            label="output"
+                            fullWidth
+                            multiline
+                            rows={7}
+                            variant="outlined"
+                            value={outputText}
+                        />
+                    </Grid>
+                    <Grid item xs={8}>
+                        <TextField
+                            label="input"
+                            fullWidth
+                            value={inputText}
+                            onChange={event => setInputText(event.target.value)}
+                        />
+                    </Grid>
+                    <Grid item xs={4}>
+                        <Grid item xs={12}>
+                            <TextField
+                                label="inner"
+                                fullWidth
+                                value={innerChar}
+                                onChange={event => setInnerChar(event.target.value)}
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                label="outer"
+                                fullWidth
+                                value={outerChar}
+                                onChange={event => setOuterChar(event.target.value)}
+                            />
+                        </Grid>
+                    </Grid>
                 </Grid>
-                <Grid item xs={12}>
-                    <TextField
-                        label="outer"
-                        fullWidth
-                        margin="normal"
-                        value={outerChar}
-                        onChange={event => setOuterChar(event.target.value)}
-                    />
-                </Grid>
-            </Grid>
-        </Grid>
+            </CardContent>
+        </Card>
     )
 }

0beb81ea112b103e80ea437907517851c8dc01da
Dmitry Saltykov <25DimoN25@spark-mail.ru>
2020-05-13 03:28:27 +0300

    Updates

---
+++ precache-manifest.990ce149cd96f3e5f050c8f28822e7bb.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07359543e5904e16d8caf536f5adac33",
+    "url": "/emoji_text_generator/index.html"
+  },
+  {
+    "revision": "34f8cbe1bbb66d8ee306",
+    "url": "/emoji_text_generator/static/js/2.143fa5b7.chunk.js"
+  },
+  {
+    "revision": "d665c1a93b32348129c8",
+    "url": "/emoji_text_generator/static/js/main.1b4e8012.chunk.js"
+  },
+  {
+    "revision": "76b46a476762c760dcb7",
+    "url": "/emoji_text_generator/static/js/runtime~main.ce804f2b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/emoji_text_generator/precache-manifest.b72236eb5847c491ef7bd971cb43f36d.js"
+  "/emoji_text_generator/precache-manifest.990ce149cd96f3e5f050c8f28822e7bb.js"
 );
 
 self.addEventListener('message', (event) => {

6c40aef66f143013c051588f97b925cb7ae11aaf
Dmitry Saltykov <25DimoN25@spark-mail.ru>
2020-05-13 03:09:25 +0300

    new border function (wip) & copy on click

--- src/components/Application.js
+++ src/components/Application.js
@@ -5,23 +5,39 @@ import {charTable} from "constants/charTable"
 import Card from "@material-ui/core/Card"
 import CardContent from '@material-ui/core/CardContent'
 import makeStyles from "@material-ui/core/styles/makeStyles"
+import FormControlLabel from "@material-ui/core/FormControlLabel"
+import Checkbox from "@material-ui/core/Checkbox"
 import _ from "lodash"
+import Popover from "@material-ui/core/Popover";
+import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles({
     card: {
         maxWidth: 800,
         backgroundColor: "#f0ffef"
-    }
+    },
+    popover: {
+        padding: 30,
+    },
 })
 
 export default function Application() {
 
+    const [withBorder, setWithBorder] = useState(true)
     const [innerChar, setInnerChar] = useState("█")
     const [outerChar, setOuterChar] = useState("░")
     const [inputText, setInputText] = useState("жесть")
     const [outputText, setOutputText] = useState("")
 
+    const [popoverTarget, setPopoverTarget] = useState(null);
+
     const classes = useStyles();
+
+    const outputTextClickHandler = event => {
+        event.target.select()
+        document.execCommand("Copy")
+        setPopoverTarget(event.currentTarget);
+    }
 
     useEffect(() => {
         const arrayOfCharsArrays = inputText
@@ -31,12 +47,15 @@ export default function Application() {
 
         const rowArrayToString = rowArray => rowArray.map(flag => flag ? innerChar : outerChar).join("")
 
-        const newOutputText = _.zip(...arrayOfCharsArrays)
-            .map(rowArrays => outerChar + rowArrays.map(rowArrayToString).join(outerChar) + outerChar)
-            .join("\n")
+        const rows = _.zip(...arrayOfCharsArrays)
+            .map(rowArrays => rowArrays.map(rowArrayToString).join(outerChar))
+
+        const newOutputText = (
+            withBorder ? rows.map(row => outerChar + row + outerChar) : rows
+        ).join("\n")
 
         setOutputText(newOutputText)
-    }, [innerChar, outerChar, inputText])
+    }, [withBorder, innerChar, outerChar, inputText])
 
     return (
         <Card variant="outlined" className={classes.card}>
@@ -50,7 +69,23 @@ export default function Application() {
                             rows={7}
                             variant="outlined"
                             value={outputText}
+                            onClick={outputTextClickHandler}
                         />
+                        <Popover
+                            open={Boolean(popoverTarget)}
+                            anchorEl={popoverTarget}
+                            onClose={() => setPopoverTarget(null)}
+                            anchorOrigin={{
+                                vertical: 'center',
+                                horizontal: 'center',
+                            }}
+                            transformOrigin={{
+                                vertical: 'center',
+                                horizontal: 'center',
+                            }}
+                        >
+                            <Typography className={classes.popover}>Copied!</Typography>
+                        </Popover>
                     </Grid>
                     <Grid item xs={8}>
                         <TextField
@@ -59,24 +94,30 @@ export default function Application() {
                             value={inputText}
                             onChange={event => setInputText(event.target.value)}
                         />
+                        <FormControlLabel
+                            label="use border"
+                            control={
+                                <Checkbox
+                                    color="primary"
+                                    checked={withBorder}
+                                    onChange={event => setWithBorder(event.target.checked)}
+                                />
+                            }
+                        />
                     </Grid>
                     <Grid item xs={4}>
-                        <Grid item xs={12}>
-                            <TextField
-                                label="inner"
-                                fullWidth
-                                value={innerChar}
-                                onChange={event => setInnerChar(event.target.value)}
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <TextField
-                                label="outer"
-                                fullWidth
-                                value={outerChar}
-                                onChange={event => setOuterChar(event.target.value)}
-                            />
-                        </Grid>
+                        <TextField
+                            label="inner"
+                            fullWidth
+                            value={innerChar}
+                            onChange={event => setInnerChar(event.target.value)}
+                        />
+                        <TextField
+                            label="outer"
+                            fullWidth
+                            value={outerChar}
+                            onChange={event => setOuterChar(event.target.value)}
+                        />
                     </Grid>
                 </Grid>
             </CardContent>

f3013106f4546ee008b522893fd9c84a7831abbd
Dmitry Saltykov <25DimoN25@spark-mail.ru>
2020-05-14 14:41:39 +0300

    new copy logic & design

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,43 +3,58 @@ import * as R from 'ramda'
 import TextField from "@material-ui/core/TextField"
 import Grid from "@material-ui/core/Grid"
 import {charTable} from "constants/charTable"
-import Card from "@material-ui/core/Card"
-import CardContent from '@material-ui/core/CardContent'
 import makeStyles from "@material-ui/core/styles/makeStyles"
 import FormControlLabel from "@material-ui/core/FormControlLabel"
 import Checkbox from "@material-ui/core/Checkbox"
-import Popover from "@material-ui/core/Popover"
 import Typography from "@material-ui/core/Typography"
+import Paper from "@material-ui/core/Paper"
+import Card from "@material-ui/core/Card"
+import CardActionArea from "@material-ui/core/CardActionArea"
+import CardContent from "@material-ui/core/CardContent"
+import Alert from '@material-ui/lab/Alert'
+import Snackbar from "@material-ui/core/Snackbar"
 
 const useStyles = makeStyles({
-    card: {
+    paperContent: {
+        margin: 15,
+        padding: 15,
         maxWidth: 800,
         backgroundColor: "#ECEFF1"
     },
-    popover: {
-        padding: 20,
+    typographyPopover: {
+        padding: 15,
         backgroundColor: "#CFD8DC"
     },
+    typographyOutput: {
+        whiteSpace: 'pre-wrap'
+    },
+    cardOutput: {
+        backgroundColor: "#CFD8DC"
+    }
 })
 
 export default function Application() {
+    const classes = useStyles()
 
     const [withBorder, setWithBorder] = useState(true)
     const [innerChar, setInnerChar] = useState("🟥")
     const [outerChar, setOuterChar] = useState("🤍")
     const [inputText, setInputText] = useState("жесть")
     const [outputText, setOutputText] = useState("")
+    const [showCopiedAlert, setShowCopiedAlert] = useState(false)
 
-    const [popoverTarget, setPopoverTarget] = useState(null)
+    const handleCheckboxChangeWithBorder = event => setWithBorder(event.target.checked)
+    const handleTextFieldChangeInner = event => setInnerChar(event.target.value)
+    const handleTextFieldChangeOuter = event => setOuterChar(event.target.value)
+    const handleTextFieldChangeInput = event => setInputText(event.target.value)
 
-    const classes = useStyles()
+    const handleSnackbarCloseCopied = (event, reason) => {
+        if (reason !== "clickaway") setShowCopiedAlert(false)
+    }
 
-    const outputTextClickHandler = event => {
-        if (event.target.type === "textarea") {
-            event.target.select()
-            document.execCommand("Copy")
-            setPopoverTarget(event.currentTarget)
-        }
+    const handleCardClickOutput = () => {
+        navigator.clipboard.writeText(outputText)
+        setShowCopiedAlert(true)
     }
 
     useEffect(() => {
@@ -65,68 +80,60 @@ export default function Application() {
     }, [withBorder, innerChar, outerChar, inputText])
 
     return (
-        <Card className={classes.card}>
-            <CardContent>
-                <Grid container spacing={2}>
-                    <Grid item xs={12}>
-                        <TextField
-                            label="output"
-                            fullWidth
-                            multiline
-                            variant="outlined"
-                            value={outputText}
-                            onClick={outputTextClickHandler}
-                        />
-                        <Popover
-                            open={Boolean(popoverTarget)}
-                            anchorEl={popoverTarget}
-                            onClose={() => setPopoverTarget(null)}
-                            anchorOrigin={{
-                                vertical: 'center',
-                                horizontal: 'center',
-                            }}
-                            transformOrigin={{
-                                vertical: 'center',
-                                horizontal: 'center',
-                            }}
-                        >
-                            <Typography className={classes.popover}>Copied!</Typography>
-                        </Popover>
-                    </Grid>
-                    <Grid item xs={8}>
-                        <TextField
-                            label="input"
-                            fullWidth
-                            value={inputText}
-                            onChange={event => setInputText(event.target.value)}
-                        />
-                        <FormControlLabel
-                            label="use border"
-                            control={
-                                <Checkbox
-                                    color="primary"
-                                    checked={withBorder}
-                                    onChange={event => setWithBorder(event.target.checked)}
-                                />
-                            }
-                        />
-                    </Grid>
-                    <Grid item xs={4}>
-                        <TextField
-                            label="inner"
-                            fullWidth
-                            value={innerChar}
-                            onChange={event => setInnerChar(event.target.value)}
-                        />
-                        <TextField
-                            label="outer"
-                            fullWidth
-                            value={outerChar}
-                            onChange={event => setOuterChar(event.target.value)}
-                        />
-                    </Grid>
+        <Paper className={classes.paperContent} square>
+            <Grid container spacing={2}>
+                <Grid item xs={12}>
+                    <Card className={classes.cardOutput}>
+                        <CardActionArea onClick={handleCardClickOutput}>
+                            <CardContent>
+                                <Typography className={classes.typographyOutput}>
+                                    {outputText}
+                                </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                    </Card>
+                    <Snackbar
+                        open={showCopiedAlert}
+                        onClose={handleSnackbarCloseCopied}
+                        autoHideDuration={2000}
+                        anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
+                    >
+                        <Alert severity="success" variant="filled">Copied!</Alert>
+                    </Snackbar>
                 </Grid>
-            </CardContent>
-        </Card>
+                <Grid item xs={8}>
+                    <TextField
+                        label="input"
+                        fullWidth
+                        value={inputText}
+                        onChange={handleTextFieldChangeInput}
+                    />
+                    <FormControlLabel
+                        label="use border"
+                        control={
+                            <Checkbox
+                                color="primary"
+                                checked={withBorder}
+                                onChange={handleCheckboxChangeWithBorder}
+                            />
+                        }
+                    />
+                </Grid>
+                <Grid item xs={4}>
+                    <TextField
+                        label="inner"
+                        fullWidth
+                        value={innerChar}
+                        onChange={handleTextFieldChangeInner}
+                    />
+                    <TextField
+                        label="outer"
+                        fullWidth
+                        value={outerChar}
+                        onChange={handleTextFieldChangeOuter}
+                    />
+                </Grid>
+            </Grid>
+        </Paper>
     )
 }
